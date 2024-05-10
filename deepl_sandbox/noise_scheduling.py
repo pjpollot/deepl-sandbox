4978b38d3fe43c727cc19cefc4707382b0b90196
pjpollot <pollot.pierre@gmail.com>
2024-05-10 12:21:19 +0900

    add eval phase to training phase (to be tested)

--- deepl_sandbox/noise_scheduling.py
+++ deepl_sandbox/noise_scheduling.py
@@ -1,10 +1,22 @@
 import torch
 from torch import Tensor, LongTensor
 
+from enum import Enum
+
+class VarianceType(Enum):
+    NORMAL = 0
+    ADJUSTED = 1
+
 class LinearNoiseScheduler:
-    def __init__(self, start: float, end: float, max_timesteps: int):
+    def __init__(self, start: float, end: float, max_timesteps: int, reverse_variance_type: int = VarianceType.NORMAL):
         self._betas = torch.linspace(start, end, max_timesteps)
         self._alpha_bars = (1-self._betas).cumprod(0)
+        if reverse_variance_type == VarianceType.NORMAL:
+            self._variance = lambda t: self._betas[t]
+        elif reverse_variance_type == VarianceType.ADJUSTED:
+            self._variance = lambda t: (1 - self._alpha_bars[t-1]) * self._betas[t] / (1 - self._alpha_bars[t])
+        else:
+            raise ValueError("The entered reverse variance type is not implemented.")
     
     def add_noise(self, x0: Tensor, noise: Tensor, timesteps: LongTensor) -> Tensor:
         xt_list = []
@@ -12,3 +24,10 @@ class LinearNoiseScheduler:
             xt = self._alpha_bars[t].sqrt() * x + (1-self._alpha_bars[t]).sqrt() * eps
             xt_list.append(xt.unsqueeze(0))
         return torch.concat(xt_list, dim=0)
+    
+    def denoising_step(self, xt: Tensor, epsilon: Tensor, z: Tensor, t: int) -> Tensor:
+        const_1 = 1. / (1-self._betas[t]).sqrt() 
+        const_2 = self._betas[t] / (1-self._alpha_bars[t]).sqrt()
+        std = self._variance(t).sqrt() if t > 0 else 0
+        return const_1 * (xt - const_2 * epsilon) + std * z
+        
--- drafts/reverse_process/unet_training.py
+++ drafts/reverse_process/unet_training.py
@@ -16,16 +16,21 @@ from torchvision.datasets import MNIST
 
 from deepl_sandbox.noise_scheduling import LinearNoiseScheduler
 
-BATCH_SIZE = 16
+import matplotlib.pyplot as plt
+
+BATCH_SIZE = 4
 N_EPOCHS = 20
 TRAINING_TIMESTEPS = 1000
+SAMPLE_SIZE = (16, 16)
+TEST_LABELS = [1, 9]
 
 dataset_path = os.path.join(os.path.dirname(__file__), "../../data")
 checkpoint_folder_path = os.path.join(os.path.dirname(__file__), "checkpoints")
+graphs_folder = os.path.join(os.path.dirname(__file__), "training_results")
 
 forward_transform = Compose([
     ToTensor(),
-    Resize((16, 16)),
+    Resize(SAMPLE_SIZE),
     Lambda(lambda x: 2 * x.to(torch.float32) / 255 - 1),
     Lambda(lambda z: torch.clamp(z, -1, 1)),
 ])
@@ -45,7 +50,7 @@ if __name__ == "__main__":
         download=True,
     )
     train_loader = DataLoader(training_data, BATCH_SIZE, shuffle=True)
-    unet = UNet2DModel((32, 32), in_channels=1, out_channels=1, class_embed_type="timestep")
+    unet = UNet2DModel(SAMPLE_SIZE, in_channels=1, out_channels=1, class_embed_type="timestep")
     noise_scheduler = LinearNoiseScheduler(1e-4, 0.02, TRAINING_TIMESTEPS)
     optimizer = torch.optim.AdamW(unet.parameters())
     # send to fastest device
@@ -55,18 +60,22 @@ if __name__ == "__main__":
     # save unet config 
     os.makedirs(checkpoint_folder_path, exist_ok=True)
     unet.save_config(checkpoint_folder_path)
+    # graphing
+    fig, axs = plt.subplots(ncols=len(TEST_LABELS), figsize=(len(TEST_LABELS) * 4, 4))
+    os.makedirs(graphs_folder, exist_ok=True)
     # Training phase
-    unet.train()
     cumul_mse = 0.0
-    training_steps = 0
+    training_step = 0
+    test_labels = torch.tensor([lab for lab in TEST_LABELS], device=accelerator.device, dtype=torch.long)
     for epoch in range(N_EPOCHS):
         desc = f"epoch: {epoch+1}/{N_EPOCHS}"
         progbar = tqdm(
             train_loader,
             desc=f"epoch: {epoch+1}/{N_EPOCHS}",
         )
-        for i, (x0, labels) in enumerate(progbar):
-            training_steps += 1
+        for x0, labels in progbar:
+            unet.train()
+            training_step += 1
             optimizer.zero_grad()
             timesteps = torch.randint_like(labels, TRAINING_TIMESTEPS)
             epsilon = torch.randn_like(x0)
@@ -76,7 +85,21 @@ if __name__ == "__main__":
             loss.backward()
             optimizer.step()
             cumul_mse += loss.item()
-            progbar.set_postfix({"avg MSE": cumul_mse / training_steps})
+            progbar.set_postfix({"avg MSE": cumul_mse / training_step})
+            # eval step
+            unet.eval()
+            xt_eval = torch.randn((len(TEST_LABELS), *xt.shape[1:]), device=accelerator.device)
+            t = TRAINING_TIMESTEPS - 1
+            while t >= 0:
+                z = torch.randn_like(xt_eval, device=accelerator.device)
+                ts = torch.tensor([t for _ in TEST_LABELS], device=accelerator.device, dtype=torch.long)
+                eps_eval = unet(xt_eval, ts, test_labels).sample
+                xt_eval = noise_scheduler.denoising_step(xt_eval, eps_eval, z, t)
+                t -= 1
+            for x, ax in zip(xt_eval, axs):
+                img = reverse_transform(x)
+                ax.clear()
+                ax.imshow(img)
+            fig.savefig(os.path.join(graphs_folder, f"gen_{training_step}.png"))
         # save checkpoint
         unet.save_pretrained(checkpoint_folder_path)
-            
